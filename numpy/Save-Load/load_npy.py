0bd38be0a32ef574728448e24d42ff080afe55e0
Md Jahid Hasan <jahidnoyon36@gmail.com>
2019-10-13 05:10:47 +0600

    Create load_npy.py

---
+++ numpy/Save-Load/load_npy.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+
+data_path = r'C:\Users\Sphinx\Desktop\numpy\Data\church_outdoor_train_lmdb_color.npy'
+data = np.load(data_path)
+
+print(data.shape)
+
+
